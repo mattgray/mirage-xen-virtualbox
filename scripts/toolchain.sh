a5ab53446148a663c64f8f129b37cfc2d82c2136
Matt Gray <matt.gray@7digital.com>
2015-02-15 11:27:30 +0000

    install opam / ocaml and dnsmasq/avahi

--- scripts/toolchain.sh
+++ scripts/toolchain.sh
@@ -1,5 +1,10 @@
-apt-get install -y build-essential m4 git vim
-apt-get install -y libssl-dev pkg-config
+apt-get install -y build-essential
+apt-get install -y m4
+apt-get install -y make
+apt-get install -y vim
+apt-get install -y git
+apt-get install -y libssl-dev
+apt-get install -y pkg-config
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:avsm/ocaml41+opam12
 apt-get update
