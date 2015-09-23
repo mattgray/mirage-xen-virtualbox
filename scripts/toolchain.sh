f256d1c98a6b4e2e84e6aca2c3b953dba21360b5
Matt Gray <matt.gray@7digital.com>
2015-09-23 17:06:45 +0100

    ocaml 4.02.3

--- scripts/toolchain.sh
+++ scripts/toolchain.sh
@@ -5,6 +5,6 @@ apt-get install -y git
 apt-get install -y libssl-dev
 apt-get install -y pkg-config
 apt-get install -y software-properties-common
-add-apt-repository -y ppa:avsm/ocaml41+opam12
+add-apt-repository -y ppa:avsm/ocaml42+opam12
 apt-get update
 apt-get install -y opam ocaml-native-compilers camlp4-extra
